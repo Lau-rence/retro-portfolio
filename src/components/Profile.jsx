f505a06b1957110acbfec25dcdf5cb1873ef04cb
Laurence Santos <lsantos@highlysucceed.com>
2023-10-07 12:22:22 +0800

    window component style changed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,15 @@
-import { Container, Box } from "@chakra-ui/react";
+import { Container, Box, Text } from "@chakra-ui/react";
 
 //Component
 import WindowComponent from "./WindowComponent";
 
 const Profile = () => {
     return ( 
-        <Container 
+        <Box 
             as={'section'}
         >
-            <WindowComponent title={'Title'} content={'Content'}/>
-        </Container>
+            <WindowComponent title={'Title'} content={<Text color={'white'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</Text>}/>
+        </Box>
      );
 }
  
--- src/components/WindowComponent.jsx
+++ src/components/WindowComponent.jsx
@@ -1,35 +1,70 @@
 import React from 'react';
-import { Box, Button, Text } from '@chakra-ui/react';
-import { SmallCloseIcon } from '@chakra-ui/icons';
+import { Box, Button, Icon, Text } from '@chakra-ui/react';
+import { CloseIcon } from '@chakra-ui/icons';
+import { BiWindow } from "react-icons/bi";
 
-const CmdStyleWindow = ({ title, content, onClose, onMinimize, onMaximize, isMaximized }) => {
+const WindowComponent = ({ title, content, onClose }) => {
   return (
     <Box
-      bg="black"
-      color="white"
-      width={400}
-      height={300}
-      fontFamily="Courier New, monospace"
-      fontSize="sm"
+      bg={"black"}
+      fontSize={"sm"}
       border={'1.5px solid black'}
+      fontFamily={"Courier New, monospace"}
     >
       <Box
-        bg="white"  // Add a white background to the title area
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        border={'2px solid gray'}
+        bg={"white"}
+        display={"flex"}
+        alignItems={"center"}
+        border={'1.6px solid gray'}
+        justifyContent={"space-between"}
       >
-        <Text color={'black'}>{title}</Text>
-        <Box height={'100%'} borderLeft={'2px solid gray'} cursor={'pointer'}>
-          <Button bgColor={'transparent'} size="xs" mx={1} onClick={onClose}>
-            X
-          </Button>
+        <Box
+            width={'24px'}
+            height={'24px'}
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            borderRight={'1.6px solid gray'}
+        >
+            <Icon as={BiWindow} boxSize={4}></Icon>
+        </Box>
+        <Box
+            pl={2}
+            width={'100%'} 
+            height={'24px'}
+            display={'flex'}
+            alignItems={'center'}
+            borderX={'1.6px solid gray'}
+        >
+            <Text
+                color={'black'}
+            >
+                {title}
+            </Text>
+        </Box>
+        <Box
+            height={'100%'} 
+            cursor={'pointer'}
+            borderLeft={'1.6px solid gray'} 
+        >
+            <Button 
+                mx={1}
+                size={"xs"}
+                onClick={onClose}
+                bgColor={'transparent'} 
+            >
+                <CloseIcon
+                    boxSize={2.5}
+                />
+            </Button>
         </Box>
       </Box>
-      <Box p={2}>{content}</Box>
+      <Box>
+        {content}
+      </Box>
+      
     </Box>
   );
 };
 
-export default CmdStyleWindow;
+export default WindowComponent;
